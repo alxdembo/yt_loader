f2550e0129a067d25a9e289b4da6fb307cef621d
alex <a.dembo@ya.ru>
2020-02-01 20:16:41 +0300

    add ooyala helper

--- settings.py
+++ settings.py
@@ -4,6 +4,14 @@ URL_EXPIRATION_INTERVAL = 3600
 TMP_FOLDER = 'tmp'
 AWS_REGION = 'eu-west-2'
 
+STREAM_HOST_URIS = {
+    'youtube': 'https://www.youtube.com/watch?v=',
+    'ooyala': 'http://85072-c.ooyala.com/'
+}
+
+YOUTUBE_URI = 'https://www.youtube.com/watch?v='
+OOYALA_URI = 'http://85072-c.ooyala.com/'
+
 try:
     from .settings_local import *
 except ImportError:
--- slicer/download_helpers.py
+++ slicer/download_helpers.py
@@ -1,7 +1,19 @@
+import requests
+from flask import current_app
+
+
 class DownloadHelpers:
     @staticmethod
     def download_youtube(video_id):
         from pytube import YouTube
-        video = YouTube(f'https://www.youtube.com/watch?v={video_id}', defer_prefetch_init=False)
+        video = YouTube(current_app.config['STREAM_HOST_URIS']['youtube'] + video_id, defer_prefetch_init=False)
         video.streams.first().download()
 
+    @staticmethod
+    def download_ooyala(video_id):
+        r = requests.get(
+            f"{current_app.config['STREAM_HOST_URIS']['ooyala'] + video_id}/{current_app.config['OOYALA_TOKEN']}",
+            stream=True)
+        with open('file.mp4', 'wb') as f:
+            for chunk in r.iter_content(256):
+                f.write(chunk)
