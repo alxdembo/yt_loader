7601d65d7aa93acddbfdeab6f28475e789f6c58b
alex <a.dembo@ya.ru>
2020-02-02 12:28:34 +0300

    refactor get parameters

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pytimeparse.timeparse import timeparse
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 from slicer.slicer import Slicer
 
@@ -8,11 +8,14 @@ app.config.from_pyfile('settings.py')
 app.config.from_pyfile('settings_local.py')
 
 
-@app.route('/slicer/api/v1.0/slice/<string:source>/<string:video_id>/<string:start>/<string:end>', methods=['GET'])
-def get_slice(source, video_id, start, end):
+@app.route('/slicer/api/v1.0/slice/<string:source>/<string:video_id>', methods=['GET'])
+def get_slice(source, video_id):
+    start = request.args.get("start")
+    end = request.args.get("end")
+    rerender = "true" == request.args.get("rerender", "true")
 
     try:
-        slicer = Slicer(source, video_id, timeparse(start), timeparse(end))
+        slicer = Slicer(source, video_id, timeparse(start), timeparse(end), rerender)
         url = slicer.get_url()
 
     except ValueError as e:
