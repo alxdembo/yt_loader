918a091eac7625cdbc3ac987ee5d381476468104
alex <a.dembo@ya.ru>
2020-02-02 23:38:23 +0300

    remove unused settings

--- settings.py
+++ settings.py
@@ -6,6 +6,3 @@ AWS_REGION = 'eu-west-2'
 
 STREAM_HOST_YOUTUBE = 'https://www.youtube.com/watch?v='
 STREAM_HOST_OOYALA = 'http://85072-c.ooyala.com/'
-
-YOUTUBE_URI = 'https://www.youtube.com/watch?v='
-OOYALA_URI = 'http://85072-c.ooyala.com/'
