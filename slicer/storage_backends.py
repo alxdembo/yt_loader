750cebb740ded68389e1287849d627a2daaec925
alex <a.dembo@ya.ru>
2020-02-02 03:03:48 +0300

    fix s3 parameter misplacement

--- slicer/storage_backends.py
+++ slicer/storage_backends.py
@@ -5,7 +5,7 @@ from utils.s3_base import get_s3_resource, get_s3_client
 def get_s3_upload_url(file_name):
     get_s3_resource().Object(
         current_app.config['BUCKET_NAME'],
-        f"{current_app.config['TMP_FOLDER']}/sliced_{file_name}").upload_file(Filename="sliced_" + file_name)
+        "sliced_" + file_name).upload_file(Filename=f"{current_app.config['TMP_FOLDER']}/sliced_{file_name}")
 
     return get_s3_client().generate_presigned_url('get_object',
                                                   Params={'Bucket': current_app.config['BUCKET_NAME'],
