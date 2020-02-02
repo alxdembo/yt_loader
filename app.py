c125f2673b4cc3364b1e166a94ca02d01f1647c4
alex <a.dembo@ya.ru>
2020-02-02 09:53:39 +0300

    change input timestamps from int to timestamp

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pytimeparse.timeparse import timeparse
 from flask import Flask, jsonify
 
 from slicer.slicer import Slicer
@@ -7,10 +8,9 @@ app.config.from_pyfile('settings.py')
 app.config.from_pyfile('settings_local.py')
 
 
-@app.route('/slicer/api/v1.0/slice/<string:source>/<string:video_id>/<int:start>/<int:end>', methods=['GET'])
+@app.route('/slicer/api/v1.0/slice/<string:source>/<string:video_id>/<string:start>/<string:end>', methods=['GET'])
 def get_slice(source, video_id, start, end):
-    slicer = Slicer(source, video_id, start, end)
-
+    slicer = Slicer(source, video_id, timeparse(start), timeparse(end))
     return jsonify({'url': slicer.get_url()})
 
 
